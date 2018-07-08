3a383ce8f39aa4d278e687aa4ea0c01acecb075b
MehdiZebarjadan <MehdiZebarjadan@yahoo.com>
2018-07-08 18:57:06 -0400

    Observables & Component Communication
    https://github.com/Reactive-Extensions/RxJS/blob/master/doc/api/subjects/behaviorsubject.md

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LogFormComponent } from './components/log-form/log-form.component';
 import { LogsComponent } from './components/logs/logs.component';
 import { LogService } from './services/log.service';
+
 
 @NgModule({
   declarations: [
@@ -15,7 +17,8 @@ import { LogService } from './services/log.service';
     LogsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [LogService],
   bootstrap: [AppComponent]
--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 
 import { Log } from '../models/Log';
 
 @Injectable()
 export class LogService {
   logs: Log[];
+
+  private logSource = new BehaviorSubject<Log>({id: null, text: null, date: null});
+  selectedLog = this.logSource.asObservable();
 
   constructor() { 
     this.logs = [
@@ -14,8 +18,12 @@ export class LogService {
     ]
   }
 
-  getLogs() {
-    return this.logs;
+  getLogs(): Observable<Log[]> {
+    return of(this.logs);
+  }
+
+  setFormLog(log: Log) {
+    this.logSource.next(log);
   }
 
 }
