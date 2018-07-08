78067dbb2e3afa4891103667dc79724c1961adad
MehdiZebarjadan <MehdiZebarjadan@yahoo.com>
2018-07-08 17:03:28 -0400

    DevLogger - Setup
    - Components and Bootstrap Setup
    ng new devlogger
    cd devlogger
    
    ng g c components/navbar —spec false
    ng g c components/log-form —spec false
    ng g c components/logs —spec false
    
    npm install bootstrap@4.0.0-beta.2 popper.js jquery font-awesome
    remove the ^ character in front of the bootstrap version in Package.json
    
    - Building the UI
    - Logs Components & Service
    create a log service
    ng g s services/log --module=app.module

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./logs.component.css']
 })
 export class LogsComponent implements OnInit {
+  logs: {
+    id: string,
+    text: string,
+    date: any
+  }[];
 
   constructor() { }
 
   ngOnInit() {
+    this.logs = [
+      {id: '1', text: 'Generated components', date: new Date('12/26/2017 12:54:23')},
+      {id: '2', text: 'Added Bootstrap', date: new Date('12/27/2017 9:33:13')},
+      {id: '3', text: 'Added logs component', date: new Date('12/27/2017 12:00:23')}
+    ]
   }
 
 }
